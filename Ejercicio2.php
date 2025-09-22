e6063ecc181fa06d0f4eda6f2584235725191bca
Sergiogp7 <sergio.garciapedrero7@gmail.com>
2025-09-22 10:32:42 +0200

    Add files via upload

---
+++ Ejercicio2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <?php
+    $direccion = "Calle Falsa 123";
+    $numero = "123456789";
+    echo "Dirección: $direccion<br>";
+    echo "Número: $numero";
+    ?>
+</body>
