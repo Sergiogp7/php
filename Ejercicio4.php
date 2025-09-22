493a2640085a49c19b9ad98c571a7bf71c11d92f
Sergiogp7 <sergio.garciapedrero7@gmail.com>
2025-09-22 10:58:41 +0200

    Add files via upload

---
+++ Ejercicio4.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <?php
+    echo "<table border=1>
+                    <tr>
+                    <th></th>
+                        <th>Lunes</th>
+                        <th>Martes</th>
+                        <th>Miercoles</th>
+                        <th>Jueves</th>
+                        <th>Viernes</th>
+                    </tr>
+                    <tr>
+                    <th>8:15-9:15</th>
+                        <td>Diseño de Interfaces</td>
+                        <td rowspan=2>Programacion de Cliente</td>
+                        <td rowspan=2>Diseño de Interfaces</td>
+                        <td rowspan=2>Ingles Profesional</td> 
+                        <td rowspan=2>Diseño de Interfaces</td>
+                    </tr>
+                    <tr>
+                    <th>9:15-10:15</th>
+                        <td rowspan=2>Optativa GS</td>
+                    </tr>
+                    <tr>
+                    <th>10:15-11:15</th>
+                        <td>IPE</td>
+                        <td>IPE</td>
+                        <td>Desarrollo Web</td>
+                        <td>Programacion de Servidor</td>
+                    </tr>
+                    <tr>
+                    
+                    <th>11:15-11:45</th>
+                        <th colspan=5>Recreo</th>
+                    </tr>
+                    <tr>
+                    <th>11:45-12:45</th>
+                        <td rowspan=3>Programacion de servidor</td>
+                        <td>IPE</td>
+                        <td rowspan=2>Programacion de Servidor</td>
+                        <td>Desarrollo Web</td>
+                        <td>Programacion de Servidor</td>
+                    </tr>
+                    <tr>
+                    <th>12:45-13:45</th>
+                        <td rowspan=2>Proyecto Integrado</td>
+                        <td rowspan=2>Programacion de Cliente</td>
+                        <td rowspan=2>Programacion de Cliente</td>
+                    </tr>
+                    <tr>
+                    <th>13:45-14:45</th>
+                        <td>Optativa GS</td>
+                    </tr>
+                </table>";
+    ?>
+</body>
+</html>
