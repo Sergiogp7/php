c50da37710f36e04922597cf512e84cfe6106a58
Sergiogp7 <sergio.garciapedrero7@gmail.com>
2025-09-22 11:13:32 +0200

    Add files via upload

---
+++ Ejercicio5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <meta charset="UTF-8">
+        </head>
+        <body>
+            <?php
+                $x = 144;
+                $y = 999;
+                echo "Suma: ", ($x + $y), "<br>";
+                echo "Resta: ", ($x - $y), "<br>";
+                echo "División: ", ($x / $y), "<br>";
+                echo "Multiplicación: ", ($x * $y), "<br>";
+            ?>
+        </body>
+</html>
